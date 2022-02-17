d014cd07ea9e4370cebd399f2d83553808caccbd
iamyogeshmalkani <conan.yogesh@gmail.com>
2022-02-17 23:06:37 +0530

    map api

--- src/App.js
+++ src/App.js
@@ -12,6 +12,23 @@ export default function App() {
       <Navbar />
       <About />
       <Projects />
+      {/* <section id="projects" className="text-gray-400 bg-gray-900 body-font flex flex-wrap" >
+      <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+      width="560" height="315"
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+<iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+width="560" height="315"
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+/>
+        </section> */}
+    
       <Skills />
       
       <Contact />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,8 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  
 
   function encode(data) {
     return Object.keys(data)
@@ -13,15 +12,16 @@ export default function Contact() {
       .join("&");
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+  function handleSubmit(event) {
+      
+    event.preventDefault();
+    emailjs.sendForm('service_w9o9jmw','template_teurkuj',event.target,'user_MSXKyBQMBtm1TpUnXv12P')
+    .then(res=>{console.log(res)
+      if(res.status==200){
+        alert("mMessage Sent To Sagar");
+      }
     })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    .catch(err=>console.log(err));
   }
 
   return (
@@ -59,14 +59,14 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed text-gray-100">9873328764</p>
+              <p className="leading-relaxed text-gray-100">+ 91 9873328764</p>
             </div>
           </div>
         </div>
         <form
           netlify
           name="contact"
-          onSubmit={handleSubmit}
+          onSubmit={(e)=>{handleSubmit(e)}}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
@@ -84,7 +84,7 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              // onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -96,7 +96,7 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              // onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -109,7 +109,7 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              // onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
