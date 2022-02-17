d5648a15917eae1db473d80c6e7a204a63e27a62
iamyogeshmalkani <conan.yogesh@gmail.com>
2022-02-17 21:11:16 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,60 @@
+import gorilla from "./gorillaprint.jpeg";
+import illustration1 from "./Illustration1.jpeg";
+import bookcover from "./bookcover.jpeg";
+import goat from "./Thegoatcamp.jpeg";
+import truck from "./Truck.jpeg";
+import virat from "./virat.jpeg";
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: gorilla,
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: bookcover,
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: goat,
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: truck,
+      link: "https://pythonbootcamp.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: illustration1,
+        link: "https://pythonbootcamp.com",
+      },
+      {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: virat,
+        link: "https://pythonbootcamp.com",
+      },
+     
+     
+  ];
+  export const skills =["Adobe Photoshop","Adobe Illustrator","Adobe Premier Pro","Adobe Indesign", "Coral Draw", "Adobe After Effect"];
